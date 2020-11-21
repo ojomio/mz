991287a4d85f7f7a6608a25ef6433cda1599f952
Юрий Пайков <cry5tal@cry5tal.in>
2020-11-21 18:57:04 +0500

    Example app

---
+++ print.py
@@ -0,0 +1,41 @@
+import os  # Отсюда нам понадобятся методы для отображения содержимого директорий
+import sys  # sys нужен для передачи argv в QApplication
+
+from PyQt5 import QtWidgets
+
+from qt import mainwindow  # Это наш конвертированный файл дизайна
+
+
+class ExampleApp(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.btnBrowse.clicked.connect(
+            self.browse_folder
+        )  # Выполнить функцию browse_folder
+        # при нажатии кнопки
+
+    def browse_folder(self):
+        self.listWidget.clear()  # На случай, если в списке уже есть элементы
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
+        # открыть диалог выбора директории и установить значение переменной
+        # равной пути к выбранной директории
+
+        if (
+            directory
+        ):  # не продолжать выполнение, если пользователь не выбрал директорию
+            for file_name in os.listdir(directory):  # для каждого файла в директории
+                self.listWidget.addItem(file_name)  # добавить файл в listWidget
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = ExampleApp()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec_()  # и запускаем приложение
+
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
