befced7d49ce909dbb630da269d039f5be191e67
Юрий Пайков <cry5tal@cry5tal.in>
2020-11-22 13:57:51 +0500

    Load data for preview

--- print.py
+++ print.py
@@ -1,8 +1,12 @@
-import os  # Отсюда нам понадобятся методы для отображения содержимого директорий
+import itertools
 import sys  # sys нужен для передачи argv в QApplication
 from pathlib import Path
+from typing import Optional
 
+import openpyxl
+from openpyxl.worksheet.worksheet import Worksheet
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QTableWidgetItem
 
 from qt import mainwindow  # Это наш конвертированный файл дизайна
 
@@ -13,24 +17,67 @@ class ExampleApp(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         # и т.д. в файле design.py
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.buttonBox.hide()
         self.btnBrowse.clicked.connect(
             self.browse_folder
         )  # Выполнить функцию browse_folder
         # при нажатии кнопки
 
+        self.buttonBox.accepted.connect(self.process_file)
+        # self.buttonBox.rejected.connect(self.process_file)
+
+        self.file: Optional[Path] = None
+
     def browse_folder(self):
-        self.listWidget.clear()  # На случай, если в списке уже есть элементы
+        self.buttonBox.hide()
+        self.tableWidget.clear()  # На случай, если в списке уже есть элементы
         dialog = QtWidgets.QFileDialog(self)
         (filename, _) = dialog.getOpenFileName(
             dialog, "Выберите таблицу", filter="Таблицы (*.xls *.xlsx)"
         )
 
+        self._handle_browse_result(filename)
+
+    def _handle_browse_result(self, filename: Optional[str]):
         if not filename:
             return
-        file = Path(filename)
+
+        self.file = Path(filename)
+
+        self.wb = openpyxl.open(self.file)
+        self.ws = self.wb.active
 
         self.buttonBox.setEnabled(True)
-        self.setWindowTitle(f'Active - {file.name}')
+        self.setWindowTitle(f'Active - {self.file.name}')
+        self._load_table_widget(self.ws)
+        self.buttonBox.show()
+
+    def _load_table_widget(self, ws: Worksheet):
+        headers = [
+            cell.value for cell in itertools.takewhile(lambda x: bool(x), ws['1'])
+        ]
+        self.tableWidget.setColumnCount(len(headers))
+        self.tableWidget.setHorizontalHeaderLabels(headers)
+
+        data = ws.iter_rows(min_row=2)
+        for x, row in enumerate(data):
+            if row[0].value is None:
+                break
+
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for y, cell in enumerate(row):
+                val = cell.value
+                if val is None:
+                    continue
+                item = QTableWidgetItem(str(val))
+                self.tableWidget.setItem(x, y, item)
+
+        self.range_start.setText('A1')
+        self.range_end.setText(f'A{ws.max_row}')
+
+    def process_file(self):
+        if not self.file:
+            return
 
 
 def main():
