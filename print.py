20c060c59c4b97f14250c84b33dc686858428326
Юрий Пайков <cry5tal@cry5tal.in>
2020-11-23 01:10:59 +0500

    Added processing

--- print.py
+++ print.py
@@ -1,12 +1,16 @@
 import itertools
+import re
 import sys  # sys нужен для передачи argv в QApplication
 from pathlib import Path
 from typing import Optional
 
 import openpyxl
+from openpyxl.utils import coordinate_to_tuple
 from openpyxl.worksheet.worksheet import Worksheet
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QTableWidgetItem
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QDesktopServices
+from PyQt5.QtWidgets import QFileDialog, QTableWidgetItem
 
 from qt import mainwindow  # Это наш конвертированный файл дизайна
 
@@ -33,7 +37,10 @@ class ExampleApp(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         self.tableWidget.clear()  # На случай, если в списке уже есть элементы
         dialog = QtWidgets.QFileDialog(self)
         (filename, _) = dialog.getOpenFileName(
-            dialog, "Выберите таблицу", filter="Таблицы (*.xls *.xlsx)"
+            dialog,
+            "Выберите таблицу",
+            options=QFileDialog.DontUseNativeDialog,
+            filter="Таблицы (*.xls *.xlsx)",
         )
 
         self._handle_browse_result(filename)
@@ -42,7 +49,7 @@ class ExampleApp(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         if not filename:
             return
 
-        self.file = Path(filename)
+        self.file = Path(filename).resolve()
 
         self.wb = openpyxl.open(self.file)
         self.ws = self.wb.active
@@ -78,6 +85,35 @@ class ExampleApp(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
     def process_file(self):
         if not self.file:
             return
+
+        row_start, col_start = coordinate_to_tuple(self.range_start.text())
+        row_end, col_end = coordinate_to_tuple(self.range_end.text())
+
+        for row in range(row_start, row_end + 1):
+
+            cell_content = self.ws.cell(row, col_start).value
+            if not cell_content:
+                continue
+
+            if sheet_name_match := re.search(
+                r'^(.*/\w*?(?:[a-zа-я](?=[А-ЯA-Z])|\w$))', cell_content
+            ):
+                sheet_name = f'Печать {sheet_name_match.group(1).replace("/", " ")}'
+                print(f'Adding {sheet_name}...')
+                new_ws = self.wb.create_sheet(sheet_name)
+                new_ws['I13'].value = cell_content
+                print('Sheet processed')
+
+        self._save_workbook()
+
+    def _save_workbook(self):
+        edited_file = (self.file.parent / f'processed_{self.file.stem}').with_suffix(
+            self.file.suffix
+        )
+        print(f'Saving to {edited_file}...')
+        self.wb.save(edited_file)
+
+        QDesktopServices.openUrl(QUrl.fromLocalFile(str(edited_file)))
 
 
 def main():
