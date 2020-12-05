690da9d51f0652c3373ec72e8cba4cc52ddfb771
Юрий Пайков <cry5tal@cry5tal.in>
2020-12-05 16:51:19 +0500

    Fix installation, reduce size

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from cx_Freeze import Executable, setup
 
 # Dependencies are automatically detected, but it might need fine tuning.
 # build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
-build_exe_options = {}
+build_exe_options = {"excludes": ["tcl", "tkinter", "asyncio", "unittest", "email"]}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -16,7 +16,8 @@ if sys.platform == "win32":
 setup(
     name="Printer",
     version="0.1",
-    description="Printtr",
+    description="Подготовка распечаток для Wildberries",
     options={"build_exe": build_exe_options},
+    packages=['qt'],
     executables=[Executable("print.py", base=base, icon='Icon.ico')],
 )
